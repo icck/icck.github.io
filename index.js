9a0d2208c242d1a17b7266e81a816365005a8b2f
icck <icck.n.n1111@gmail.com>
2019-08-28 22:19:55 +0900

    rebuilding site 2019年  8月 28日 水曜日 22:19:55 JST

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-var data = []
+var data = [
+{
+url: "https://icck.github.io/posts/000/",
+title: "Hugoブログ更新方法",
+content: "[Hugoの使い方を今後のブログ更新のために Hugoでブログを書いていく上で、運用方法をメモしていこうと思います。 インストール等は気が向いたときに Local環境を起動 $ hugo server --theme=hugo-theme-nix --buildDrafts --watch 記事の作成 $ hugo new post/test.md --- title: &amp;quot;test&amp;quot; date: 2019-08-26T01:14:23&#43;09:00 draft: true --- title:記事のタイトルに修正 date:作成時刻のため、そのまま draft:trueが非公開。falseに変更し公開 Sample --- title: &amp;quot;test&amp;quot; date: 2019-08-26T01:14:23&#43;09:00 draft: false --- ## ここにマークダウンで本文を記載 githubへpush ./deploy.sh]"
+}
+,{
+url: "https://icck.github.io/posts/",
+title: "Posts",
+content: "[]"
+}]
